bd3654d65841e6d4c97d4c464a3393806c2b1f38
Domantas Petrauskas <dom.petrauskas@gmail.com>
2022-08-08 22:37:31 +0300

    Add relevant documentation from quickstart's main process

--- main/src/main/scala/quickstart/Main.scala
+++ main/src/main/scala/quickstart/Main.scala
@@ -12,7 +12,7 @@ import scala.scalajs.js
 import scala.scalajs.js.|
 
 object Main extends App {
-
+  // Create the browser window.
   def createWindow(): Unit = {
     val mainWindow = new BrowserWindow(new BrowserWindowConfig {
       override val height = 600
@@ -21,14 +21,24 @@ object Main extends App {
         override val preload = Path.join(__dirname, "preload.js")
       }
     })
+
+    // and load the index.html of the app.
     mainWindow.loadFile("index.html")
-//    mainWindow.webContents.openDevTools()
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
   }
 
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
   app
     .whenReady()
     .`then`((_ => {
       createWindow()
+
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
       app.on(
         "activate",
         () => {
@@ -36,6 +46,10 @@ object Main extends App {
         }
       )
     }): js.Function1[Unit, Unit | js.Thenable[Unit]])
+
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
   app.on(
     "window-all-close",
     () => {
